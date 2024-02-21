a49a965ddc349aad7603e3ab09cb31c878b822ec
Stephen Taylor <stephenmtaylor93@gmail.com>
2024-02-21 12:04:17 -0500

    instance variables

--- my_car.rb
+++ my_car.rb
@@ -1,6 +1,7 @@
 class MyCar
+  attr_accessor :color
+  attr_reader :year
 
-  attr_accessor :year, :color, :model, :speed
   def initialize(year, color, model)
     @year = year
     @color = color
@@ -8,24 +9,39 @@ class MyCar
     @speed = 0
   end
 
+  def speed
+    @speed
+  end
   def speed_up(accelerate)
-    speed = speed + accelerate
+    @speed += accelerate
   end
 
   def brake(deaccelerate)
-    speed -= deaccelerate
+    @speed -= deaccelerate
   end
 
   def shut_off
-    speed = 0
+    @speed = 0
+  end
+
+  def spray_paint(color)
+    self.color = color
+    puts "color is now #{self.color} and speed is #{self.speed}"
   end
 end
 
 
 tacoma = MyCar.new(1998, 'Silver', 'Tacoma')
 
-puts tacoma.year
 puts tacoma.speed
 
 tacoma.speed_up(10)
-tacoma.speed
+puts tacoma.speed
+
+# puts tacoma.color
+# tacoma.color = "Yellow"
+# puts tacoma.color
+# puts tacoma.year
+
+tacoma.spray_paint('Black')
+puts tacoma.color
