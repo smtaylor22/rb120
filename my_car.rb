9ecd35bd15649d4193b6820b47192bd8a749be83
Stephen Taylor <stephenmtaylor93@gmail.com>
2024-02-26 12:04:31 -0500

    super class practice

--- my_car.rb
+++ my_car.rb
@@ -1,28 +1,36 @@
-class MyCar
-  attr_accessor :color, :model
+class Vehicle
+  attr_accessor :color, :model, :speed
   attr_reader :year
+  
+  @@num_of_vehicles = 0
 
   def initialize(year, color, model)
     @year = year
     @color = color
     @model = model
     @speed = 0
+    @@num_of_vehicles += 1
   end
 
-  def speed
-    @speed
-  end
-
+  @@Vehicles
   def speed_up(accelerate)
     @speed += accelerate
+    puts "Speeding up. Speed is now #{speed}"
+
   end
 
   def brake(deaccelerate)
     @speed -= deaccelerate
+    puts "Slowing down. Speed is now #{speed}"
   end
 
   def shut_off
     @speed = 0
+    puts "Speed is now #{speed}"
+  end
+
+  def self.calc_mileage(miles, gals)
+    puts "#{miles / gals} miles per gallon of gas"
   end
 
   def spray_paint(color)
@@ -30,32 +38,52 @@ class MyCar
     puts "color is now #{self.color} and speed is #{self.speed}"
   end
 
-  def self.calc_mileage(miles, gals)
-    miles / gals
-  end
-
-  def to_s
-    "This car is a #{color} #{model} from #{year}"
+  def self.print_num_vehicles
+    puts @@num_of_vehicles
   end
 end
 
 
-tacoma = MyCar.new(1998, 'Silver', 'Tacoma')
+class MyCar < Vehicle
+
+  NUM_OF_DOORS = 2
+
+  def to_s
+    "This car is a #{color} #{model} from #{year}"
+  end
+
+end
+
+
+class MyTruck < Vehicle
+  NUM_OF_DOORS = 4
+
+  def to_s
+    "This truck is a #{color} #{model} from #{year}"
+  end
+end
+
+
+tacoma = MyTruck.new(1998, 'Silver', 'Tacoma')
 
 puts tacoma.speed
 
 tacoma.speed_up(10)
-puts tacoma.speed
+tacoma.speed
 
-# puts tacoma.color
-# tacoma.color = "Yellow"
-# puts tacoma.color
-# puts tacoma.year
+puts tacoma.color
+tacoma.color = "Yellow"
+puts tacoma.color
+puts tacoma.year
 
 tacoma.spray_paint('Black')
 puts tacoma.color
 
+subaur = MyCar.new(2005, 'Red', "Forester")
 
-puts MyCar.calc_mileage(60, 12)
+MyCar.calc_mileage(60, 12)
 
 puts tacoma
+
+
+Vehicle.print_num_vehicles
