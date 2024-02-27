6981f77f4232943d792e8ce3797ce219526cb938
Stephen Taylor <stephenmtaylor93@gmail.com>
2024-02-27 12:14:27 -0500

    more practice

--- lesson_2/test.rb
+++ lesson_2/test.rb
@@ -1,15 +1,47 @@
-class MeMyselfAndI
-  p self
+class Person
+  attr_accessor :first_name, :last_name
 
-  def self.me
-    p self
+  def initialize(full_name)
+    parse_full_name(full_name)
   end
 
-  def myself
-    p self
+  def name
+    "#{first_name} #{last_name}".strip
+  end
+
+  def name=(full_name)
+    parse_full_name(full_name)
+  end
+
+  private
+
+  def parse_full_name(full_name)
+    parts = full_name.split
+    @first_name = parts.first
+    @last_name = parts.size > 1 ? parts.last : ''
+  end
+
+  def to_s
+    name
   end
 end
 
-i = MeMyselfAndI.new
-MeMyselfAndI.me
-i.myself
+# bob = Person.new('Robert')
+# p bob.name                  # => 'Robert'
+# p bob.first_name            # => 'Robert'
+# p bob.last_name             # => ''
+# bob.last_name = 'Smith'
+# p bob.name                  # => 'Robert Smith'
+
+# bob.name = "John Adams"
+# p bob.first_name            # => 'John'
+# p bob.last_name             # => 'Adams'
+
+
+bob = Person.new('Robert Smith')
+rob = Person.new('Robert Smith')
+
+p bob.name == rob.name
+
+bob = Person.new("Robert Smith")
+puts "The person's name is: #{bob}"
