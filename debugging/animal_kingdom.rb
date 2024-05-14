82bdf67ec25f9de2e7780fa391495a29acb781f1
Stephen Taylor <stephenmtaylor93@gmail.com>
2024-05-14 11:44:53 -0400

    debugging practice

---
+++ debugging/animal_kingdom.rb
@@ -0,0 +1,56 @@
+class Animal
+  def initialize(diet, superpower)
+    @diet = diet
+    @superpower = superpower
+  end
+
+  def move
+    puts "I'm moving!"
+  end
+
+  def superpower
+    puts "I can #{@superpower}!"
+  end
+end
+
+class Fish < Animal
+  def move
+    puts "I'm swimming!"
+  end
+
+  def diet
+    @diet
+  end
+end
+
+class Bird < Animal
+end
+
+class FlightlessBird < Bird
+  def diet
+    @diet
+  end
+  def move
+    puts "I'm running!"
+  end
+end
+
+class SongBird < Bird
+  def initialize(diet, superpower, song)
+    super(diet, superpower)
+    @song = song
+  end
+
+  def move
+    puts "I'm flying!"
+  end
+end
+
+# Examples
+
+unicornfish = Fish.new(:herbivore, 'breathe underwater')
+penguin = FlightlessBird.new(:carnivore, 'drink sea water')
+robin = SongBird.new(:omnivore, 'sing', 'chirp chirrr chirp chirp chirrrr')
+
+puts unicornfish.diet
+puts penguin.diet
