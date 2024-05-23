48b45736a88e91788cd96093ae3fa5873dac6837
Stephen Taylor <stephenmtaylor93@gmail.com>
2024-05-23 11:43:20 -0400

    more practice

--- test.rb
+++ test.rb
@@ -27,9 +27,18 @@ class Boat
 	end
 end
 
+# Did not require Fuel mixin
+class Bike
+  attr_reader :year
+	def initialize(year, model)
+		@year = year
+		@model = model
+	end
+end
+
 car1 = Car.new(2010, "civic")
 boat1 = Boat.new(2012, "Canoe")
+bike1 = Bike.new(2020, "Schwinn")
 p car1.fill_up
 p boat1.fill_up
-
-
+# p bike1.fill_up #error
